ef93f06dccc71e57a76e5817c4d06c612ca1a7d5
mvashishth <vashishthmayur@gmail.com>
2020-10-19 23:04:12 +0530

    completed ws chatapp

---
+++ chatapp/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Chat from './chat'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Chat />
+      </div>
+    )
+  }
+}
+
+export default App
--- node/app.js
+++ node/app.js
@@ -3,11 +3,27 @@ const fetch = require('node-fetch');
 let url = "https://www.reddit.com/r/popular.json";
 
 let settings = { method: "Get" };
+var zeta;
+var port=3000;
 
 fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
+    	zeta=json
     	for (var i=0;i<25;i++){
-    	console.log(json.data.children[i].data.subreddit)
+    	console.log(zeta.data.children[i].data.subreddit)
     }
     });
+
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send(zeta)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
